a85801bb3891b235d8bd246be86caf243ccfc474
nickreative <nicholasgoodridge@hotmail.com>
2026-02-03 22:37:43 +1000

    Update supabase.js

--- assets/js/supabase.js
+++ assets/js/supabase.js
@@ -1,4 +1,4 @@
-import { createClient } from "https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm";
+import { createClient } from "/assets/js/vendor/supabase.js";
 
 export const supabase = createClient(
   "https://tgvtmwdmwnzwqjzqxbkj.supabase.co",
